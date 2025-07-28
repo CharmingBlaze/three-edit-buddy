8634f1999765ca71e8d218944a29fa4687cb28ca
charmingblaze <jessedonedmonds@gmail.com>
2025-07-29 01:02:00 +1000

    fix(dev): Resolve server startup by setting root in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
+  root: 'src/demo',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
